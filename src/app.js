39e5e7356182e81976dca40908b3c5dd5e1660c1
Ivan Regalario <ivanregalario3@gmail.com>
2026-02-05 14:28:38 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const { connectDB } = require('./db');
-
-app.get('/', (req, res) => {
-  res.json({ message: 'API is running' });
-});
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -15,11 +11,15 @@ const interactionRoutes = require('./routes/userInteractionRoutes');
 const destinationRoutes = require("./routes/destinationRoutes");
 const recommendationRoutes = require("./routes/recommendationRoutes");
 
-
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running' });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
@@ -27,7 +27,6 @@ app.use('/api/itineraries', itineraryRoutes);
 app.use('/api/interactions', interactionRoutes);
 app.use("/api/destinations", destinationRoutes);
 app.use("/api/recommendations", recommendationRoutes);
-
 
 //route for testing
 app.get('/health', (_req, res) => res.json({ ok: true }));
